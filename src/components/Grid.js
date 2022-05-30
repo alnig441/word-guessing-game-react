c4fd71b7332f97e393216f223ec554b21741584a
alnig441 <alnig441@gmail.com>
2022-05-30 09:10:59 -0400

    click outside app handling added

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
 import Next from './Next';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, forceUpdate } from 'react';
 import './Grid.css';
 import Input from './Input';
 
@@ -8,6 +8,7 @@ export default function Grid(props) {
   let [complete, setComplete] = useState(false);
   let [score, setScore] = useState(0);
   let [sentence, setSentence] = useState('');
+  let [currentInput, setCurrentInput] = useState(0);
   const inputs = document.getElementsByTagName('input');
 
   useEffect(() => {
@@ -15,77 +16,97 @@ export default function Grid(props) {
       setSentence(props.sentence);
       setScore(0);
       setComplete(false);
+      setCurrentInput(0);
       resetGrid()
     }
   },[sentence, props.sentence])
 
+  useEffect(() => {
+       document.addEventListener('click', checkLetter, true);
+       return () => {
+           document.removeEventListener('click', checkLetter, true);
+       };
+   }, [currentInput]);
+
+   function resetGrid() {
+     for(const input of inputs) {
+       input.value = "";
+       input.removeAttribute("disabled");
+     }
+     inputs[0].focus();
+   }
+
   function checkLetter(e) {
     e.preventDefault();
-    const userInput = e.key.toLowerCase();
-    const numberOfInputs = props.sentence.length - 1;
-    const inputFieldID = parseInt(e.target.attributes['id'].value);
-    const correctLetter = props.sentence[inputFieldID].toLowerCase();
-    const thisInput = e.target;
-    const isLastInput = (inputFieldID === numberOfInputs);
-    const isFirstInput = (inputFieldID === 0);
-    const previousInput = !isFirstInput ? inputs[inputFieldID - 1] : null;
-    const nextInput = !isLastInput ? inputs[inputFieldID + 1] : null;
-
-    if(userInput.length === 1){
-      if(userInput === correctLetter){
-        thisInput.value = e.key;
-        if(isLastInput && score !== numberOfInputs) {
-          thisInput.focus();
-        } else {
-          thisInput.setAttribute('disabled', true);
-          setScore(score + 1);
-        }
-      }
-      if(!isLastInput) {
-        nextInput.focus()
-      }
+    if(!e.key) {
+      inputs[currentInput].focus();
     }
 
-    else if(userInput === 'backspace') {
-      thisInput.value = "";
-      if(previousInput.hasAttribute('disabled')) {
-        previousInput.removeAttribute("disabled");
-        if(score > 0) {
-          setScore(score - 1);
+    else {
+      const userInput = e.key.toLowerCase();
+      const numberOfInputs = props.sentence.length - 1;
+      const inputFieldID = parseInt(e.target.attributes['id'].value);
+      const correctLetter = props.sentence[inputFieldID].toLowerCase();
+      const thisInput = e.target;
+      const isLastInput = (inputFieldID === numberOfInputs);
+      const isFirstInput = (inputFieldID === 0);
+      const previousInput = !isFirstInput ? inputs[inputFieldID - 1] : null;
+      const nextInput = !isLastInput ? inputs[inputFieldID + 1] : null;
+
+      if(userInput.length === 1){
+        if(userInput === correctLetter){
+          thisInput.value = e.key;
+          if(isLastInput && score !== numberOfInputs) {
+            thisInput.focus();
+          } else {
+            thisInput.setAttribute('disabled', true);
+            setScore(score + 1);
+          }
+        }
+        if(!isLastInput) {
+          setCurrentInput(inputFieldID + 1);
+          nextInput.focus()
         }
       }
-      previousInput.focus();
-      previousInput.value = "";
-    }
 
-    if(score === numberOfInputs) {
-      setComplete(true);
-      setSentence(props.sentence);
+      else if(userInput === 'backspace' && !isFirstInput) {
+        thisInput.value = "";
+        if(previousInput.hasAttribute('disabled')) {
+          previousInput.removeAttribute("disabled");
+          if(score > 0) {
+            setScore(score - 1);
+          }
+        }
+        previousInput.focus();
+        setCurrentInput(inputFieldID - 1);
+        previousInput.value = "";
+      }
+
+      if(score === numberOfInputs) {
+        setComplete(true);
+        setSentence(props.sentence);
+      }
     }
   }
 
   if(!complete) {
     return(
+      <div id="grid">
         <Input data={buildGridData(props.sentence)} onKeyUp={checkLetter}/>
+      </div>
     )
   } else {
     return (
       <>
-        <Input data={buildGridData(props.sentence)} onKeyUp={checkLetter}/>
-        <Next onClick={props.onClick} />
+        <div id="grid">
+          <Input data={buildGridData(props.sentence)} onKeyUp={checkLetter}/>
+          <Next onClick={props.onClick} />
+        </div>
       </>
     )
   }
 }
 
-function resetGrid() {
-  const grid = document.getElementsByTagName('input');
-  for(const input of grid) {
-    input.value = "";
-    input.removeAttribute("disabled");
-  }
-  grid[0].focus();
-}
 
 function buildGridData(sentence: string) {
   const words = sentence.split(' ');
